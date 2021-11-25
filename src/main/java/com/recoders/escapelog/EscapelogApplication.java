3346870fca114be8ff03991e55205f77aa5176b9
Kyeonghye JO <81146632+shienka07@users.noreply.github.com>
2021-11-25 22:46:59 +0900

    [공통] 프로젝트 생성

---
+++ src/main/java/com/recoders/escapelog/EscapelogApplication.java
@@ -0,0 +1,13 @@
+package com.recoders.escapelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapelogApplication.class, args);
+	}
+
+}
